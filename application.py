5cb47a3741ee2384ea3c0e6aa8b68a7f055def5c
JunseoThomasPark <parkaph@bc.edu>
2022-04-27 12:57:29 -0400

    hello

---
+++ application.py
@@ -0,0 +1,59 @@
+from flask import Flask, render_template, redirect, request, url_for, jsonify
+import pymongo
+from pymongo import MongoClient
+import certifi
+import re
+from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, send, emit, join_room, leave_room
+
+application = Flask(__name__)
+application.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(application, cors_allowed_origins="*")
+cors = CORS(application)
+application.config['CORS_HEADERS'] = 'Content-Type'
+
+
+@application.route('/', methods=['GET'])
+def main():
+    return render_template('index.html')
+
+@socketio.on('message')
+def handle_message(data):
+    message = ('received message: ' + data)
+    print("message")
+    send(message, broadcast=True)
+
+@socketio.on('connect')
+def handle_connect():
+    print("connected")
+
+
+@socketio.on('json')
+def handle_json(json):
+    message = ('received json: ' + str(json))
+    print("json")
+    send(message, broadcast=True)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    message = ('received json: ' + str(json))
+    print("my event")
+    send(message, broadcast=True)
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    join_room(room)
+    send(username + ' has entered the room.', to=room)
+
+@socketio.on('leave')
+def on_leave(data):
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send(username + ' has left the room.', to=room)
+
+
+if __name__ == "__main__":
+    socketio.run(application, debug=True, port=4000)
---
+++ static/js/socket.js
@@ -0,0 +1,26 @@
+var socket = io.connect("http://localhost:4000");
+const testLabel = document.getElementById("testLabel");
+
+socket.on('connect', function() {
+        socket.emit('my event', {data: 'init connection'});
+
+    });
+
+socket.on('message', function(message) {
+        console.log(message);
+    });
+
+socket.on("join", function(message) {
+    console.log(message);
+}
+
+//every 3 seconds
+/*
+setInterval(function() {
+    socket.send("hello");
+    socket.emit('my event', {data: 'I\'m connected!'});
+}, 3000);
+*/
+
+
+
